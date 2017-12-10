71a9dce987cb4a00282ac75fae84352263d19644
mike <mikeschladt@gmail.com>
2017-12-09 20:31:37 -0500

    Added Material dependencies, Nav component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import {
+	MatButtonModule
+} from '@angular/material';
+
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+		BrowserAnimationsModule,
+    AppRoutingModule,
+		MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
